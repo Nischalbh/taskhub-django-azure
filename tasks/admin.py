e330dd58f3b0596a2b684ec9c708a21b76436305
Nischal <nischalbhattarai408@gmail.com>
2026-09-07 16:22:13 +0545

    configure task admin interface

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Task
+
 
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "user",
+        "priority",
+        "due_date",
+        "completed",
+        "created_at",
+    )
+    list_filter = (
+        "priority",
+        "completed",
+    )
+    search_fields = ("title", "user__username")
